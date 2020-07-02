50c451ff2dbf9344d52c7a006148c903c4a9892c
Zalina Magiday <57268539+zalina4813@users.noreply.github.com>
2020-07-01 22:32:07 -0400

    Node and express homework

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+var friends = [
+    {
+      name: "Tom Hardy",
+      photo:
+        "https://m.media-amazon.com/images/M/MV5BMTQ3ODEyNjA4Nl5BMl5BanBnXkFtZTgwMTE4ODMyMjE@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+      scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+      name: "Jamie Dornan",
+      photo:
+        "https://www.usmagazine.com/wp-content/uploads/2018/02/jamie-dornan-nude.jpg",
+      scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+      name: "Anthony Joshua",
+      photo:
+        "https://m.media-amazon.com/images/M/MV5BYWFlMmVhZjAtOGMzNS00MzEyLTkzMWEtN2M1MDgzODI5Njk0XkEyXkFqcGdeQXVyNTI5NjIyMw@@._V1_.jpg",
+      scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+      name: "Henry Cavill",
+      photo:
+        "https://m.media-amazon.com/images/M/MV5BMTUxNTExMzUzOF5BMl5BanBnXkFtZTgwOTI1MjA3OTE@._V1_.jpg",
+      scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+      name: "Emily Ratajowski",
+      photo:
+        "https://pmchollywoodlife.files.wordpress.com/2014/10/Emily-Ratajkowski-Reveals-Rock-Hard-Abs-In-Teeny-Crop-Top-While-Vacationing-With-Husband-gallery.jpg",
+      scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+    {
+      name: "Blake Lively",
+      photo:
+        "https://media.vanityfair.com/photos/560c07690404673e0de66e1d/master/w_960,c_limit/blake-lively-end-of-preserve-01.jpg",
+      scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+    },
+    {
+      name: "Kate Upton",
+      photo:
+        "https://m.media-amazon.com/images/M/MV5BMTk3MTA2ODQ1MF5BMl5BanBnXkFtZTcwMjkwMDYxNw@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+      scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    }
+  ];
+  module.exports = friends;
